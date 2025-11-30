c048a1f0fd5fdd85d101c5ebc9dc44bb6ae1c357
Xinshen Xiang <xinshenxiangchen@gmail.com>
2025-11-30 15:55:06 +0100

    game but the tetris class has a ton of bugs

--- BitParser.py
+++ BitParser.py
@@ -6,14 +6,17 @@ class BitParser:
 
         instructions = []
 
-        for instruction in bitcode.split("\n"):
-            line = instruction.split(" ")
-            full_instruction = utils.values[utils.get_value_from_bits(line[0])]
+        try:
+            for instruction in bitcode.split("\n"):
+                line = instruction.split(" ")
+                full_instruction = utils.values[utils.get_value_from_bits(line[0])]
 
-            for i in range(1, len(full_instruction)):
-                full_instruction[i] = full_instruction[i] + str(utils.get_value_from_bits(line[i]))
+                for i in range(1, len(full_instruction)):
+                    full_instruction[i] = full_instruction[i] + str(utils.get_value_from_bits(line[i]))
 
-            instructions.append(full_instruction)
+                instructions.append(full_instruction)
+        except IndexError:
+            print(f"Code syntax error")
 
         return instructions
 
